67d596e5c12f2c50bb2142d42f41213ff809d126
BonumTaurus <dimazostap@gmail.com>
2023-01-26 17:51:03 +0200

    delete UsersContainer, Users, users-reducer

--- my-react/src/components/Content/Content.jsx
+++ my-react/src/components/Content/Content.jsx
@@ -2,7 +2,6 @@ import style from './Content.module.css';
 import { Profile } from './Profile/Profile';
 import { DialogsContainer } from './Dialogs/DialogsContainer';
 import { Routes, Route } from 'react-router-dom';
-import { UsersContainer } from './Users/UsersContainer';
 
 const Content = (props) => {
 
@@ -12,7 +11,6 @@ const Content = (props) => {
 				<Route path={'profile'} element={<Profile />} />
 				<Route path={'dialogs'} element={<DialogsContainer />} />
 				<Route path={'dialogs/:id'} element={<DialogsContainer />} />
-				<Route path={'users'} element={<UsersContainer />} />
 			</Routes>
 		</div>
 	)
